ecbba542d1e8586241c9cc71933533b5bd5c3565
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2024-05-10 23:00:47 +0530

    Update Adjacent Characters.py

--- Adjacent Characters.py
+++ Adjacent Characters.py
@@ -1,6 +1,6 @@
 '''Given a string and we have to swap its adjacent characters(pairs).
 Here, to swap adjacent characters of a given string - we have a condition, which is "string length must be EVEN i.e. string must contains even number of characters".'''
-
+#Code-->
 st=input()
 re=''
 if len(st)%2==1:
