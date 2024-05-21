b5214831b17106b1e69e84b80dfe8731d4c88bd8
SNEHA AGARWAL <snehaag1104@gmail.com>
2024-05-21 06:25:30 +0530

    commit in past

---
+++ subtraction using if else.py
@@ -0,0 +1,7 @@
+#Program to input two numbers and subtract the smaller number from the greater number.
+a=int(input("enter the 1st no.:"))
+b=int(input("enter the 2nd no.:"))
+if (a>b):
+    print(a-b)
+else:
+    print(b-a)
