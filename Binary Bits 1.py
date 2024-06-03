fd3464feb43b9c6c6e131a5e869b9997c1dfcbcd
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2024-06-03 23:13:12 +0530

    Update Binary Bits 1.py

--- Binary Bits 1.py
+++ Binary Bits 1.py
@@ -1,11 +1,9 @@
 ''' In this challenge, you will use logical bitwise operators. All data is stored in its binary representation. The logical operators, use 1 to represent true and 0 to represent false. The logical operators compare bits in two numbers and return true or false, 0 or 1, for each bit compared.
-
 Bitwise AND operator & The output of bitwise AND is 1 if the corresponding bits of two operands is 1. If either bit of an operand is 0, the result of corresponding bit is evaluated to 0. It is denoted by &.
-
 Bitwise OR operator | The output of bitwise OR is 1 if at least one corresponding bit of two operands is 1. It is denoted by |.
-
 Bitwise XOR (exclusive OR) operator ^ The result of bitwise XOR operator is 1 if the corresponding bits of two operands are opposite. 
 Code-- '''
+
 n1, n2 = map(int, input().split())
 k = int(input())
 
