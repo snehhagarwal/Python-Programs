6bf43a9377ed293384d4fb08e1edae6cae9063d3
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2024-06-10 19:35:47 +0530

    Update Binary Burj Khalifa.py

--- Binary Burj Khalifa.py
+++ Binary Burj Khalifa.py
@@ -1,5 +1,6 @@
 ''' Write the program to create building structure like the burj khaleefa with equivalent binary numbers '''
 #Code->
+
 num = int(input())
 w = len(bin(num)) - 2
 for i in range(num+1):
