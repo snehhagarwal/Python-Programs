100424ab4608627fc594d22b32ccdeb87dad9b29
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2024-11-28 06:25:30 +0530

    commit in past

--- circle point.py
+++ circle point.py
@@ -1,5 +1,5 @@
 #Program to input the centre of a circle, radius of the circle and an arbitrary point P(x,y) and
-#determine whether the point is inside the circle, on the circle or outside the circle.
+#to determine whether the point is inside the circle, on the circle or outside the circle.
 import math
 cx=int(input("centre point of the circle:"))
 cy=int(input("centre point of the circle:"))
